d2ce59ae5844217e130b0282417e876824a45c6c
kaly-d <108493323+kaly-d@users.noreply.github.com>
2026-07-28 12:21:45 -0700

    Add variables for Logic App Consumption configuration

---
+++ DoNotUse/Terraform/Logic App Consumption/variables.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "logic_app_name" {
+  type = string
+}
