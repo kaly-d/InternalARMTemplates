3669a75f1985a4d76c318d8b348d5183a3e00aab
kaly-d <108493323+kaly-d@users.noreply.github.com>
2026-08-19 09:52:29 -0700

    Delete DoNotUse/outputs.tf

--- DoNotUse/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "sentinel_connection_id" {
-  value       = azapi_resource.sentinel_api_connection.id
-  description = "Resource ID of the Sentinel API connection"
-}
-
-output "sentinel_managed_api_id" {
-  value       = local.sentinel_managed_api_id
-  description = "Managed API ID used by the connection"
-}
